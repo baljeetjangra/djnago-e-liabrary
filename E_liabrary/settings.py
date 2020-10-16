732ad49cdda78e6107488b46832a745fa89b71a7
Baljeet Jangra <baljeetjangraa@gmail.com>
2020-10-16 17:04:23 +0530

    User registeration created

--- E_liabrary/settings.py
+++ E_liabrary/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'E_liabrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
